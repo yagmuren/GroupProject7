60bd80b1a251cac4b75ee56c794312e80cdf99e4
Karimzhon Masaitov <kmasaitov1988@gmail.com>
2021-07-07 18:52:24 -0500

    day 4 commit 3

--- src/test/java/karimzhon/June7PracticeGit.java
+++ src/test/java/karimzhon/June7PracticeGit.java
@@ -4,6 +4,7 @@ public class June7PracticeGit {
     public static void main(String[] args) {
         System.out.println("Hello class");
         System.out.println(3-2);
+        System.out.println("Code here");
 
     }
 }
